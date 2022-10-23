6f063385bc5abb1d6b5f23b188586f25c370fd61
GitHub Action <action@github.com>
2022-10-23 10:53:10 +0000

    Template cleanup

--- src/main/kotlin/com/github/lclass/kotlinandroidprojecttemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/lclass/kotlinandroidprojecttemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.lclass.kotlinandroidprojecttemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.lclass.kotlinandroidprojecttemplate.MyBundle
 
 class MyApplicationService {
 
